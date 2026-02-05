4c022437f2136fd960afee2274f0cf09fe33d26e
paginattoebooks <paginattoebooks@gmail.com>
2026-02-05 18:28:19 -0300

    Remove image from Headline component
    Removed the image element from the Headline component.

--- components/Headline.tsx
+++ components/Headline.tsx
@@ -16,7 +16,6 @@ const Headline: React.FC = () => {
           Com o <span className="text-gold">Pack Premium</span>, você garante acesso vitalício e desbloqueia todos os bônus agora.
         </p>
         <div className="mt-4 flex justify-center">
-          <img src="foto.png" alt="foto.png" className="w-full max-w-sm rounded-lg shadow-md object-cover" />
         </div>
       </div>
     </div>
